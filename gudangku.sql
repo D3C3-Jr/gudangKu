9736643d27f88400665e7afec0032abce7102b8e
Dwi Cahyono <114272278+D3C3-Jr@users.noreply.github.com>
2023-07-12 10:14:25 +0700

    update database

--- gudangku.sql
+++ gudangku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2023 at 10:56 AM
+-- Generation Time: Jul 12, 2023 at 05:13 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -46,6 +46,84 @@ INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `jenis_barang`,
 (2, 'BAR00002', 'Tinta Epson 003 Cyan', 'Jadi', '2023-07-11 03:23:23', '2023-07-11 03:23:23', '0000-00-00 00:00:00'),
 (3, 'BAR00003', 'Tinta Epson 003 Yellow', 'Jadi', '2023-07-11 08:23:26', '2023-07-11 08:23:26', '0000-00-00 00:00:00');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `id_barang_keluar` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_barang_masuk` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persediaan`
+--
+
+CREATE TABLE `persediaan` (
+  `id_barang` int(11) NOT NULL,
+  `stok_awal` int(11) NOT NULL,
+  `stok_tersedia` int(11) NOT NULL,
+  `masuk` datetime NOT NULL,
+  `keluar` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id_supplier` int(11) NOT NULL,
+  `kode_supplier` varchar(20) NOT NULL,
+  `nama_supplier` varchar(100) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `kota` varchar(100) NOT NULL,
+  `telp` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jenis_supplier` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id_supplier`, `kode_supplier`, `nama_supplier`, `alamat`, `kota`, `telp`, `email`, `jenis_supplier`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'SUP00001', 'PT. Monotaro', 'Jl. Kalimalang', 'Bekasi', '0212984479200', 'monotaro@monotaro.co.id', 'Barang', '2023-07-03 22:21:21', '2023-07-03 22:21:21', '0000-00-00 00:00:00'),
+(2, 'SUP00002', 'PT. Sangga Buana berkah', 'Jl. Raya Badami', 'Karawang', '021388578860', 'sanggabuanaberkah@sbb.co.id', 'Barang & Jasa', '2023-07-03 22:26:38', '2023-07-03 22:26:38', '0000-00-00 00:00:00'),
+(3, 'SUP00003', 'PT. Bintang Berkah Perkasa', 'Jl. Ahmad Yani', 'Karawang', '021698937877', 'bintangberkahperkasa@bbp.co.id', 'Barang & Jasa', '2023-07-10 04:02:56', '2023-07-10 04:02:56', '0000-00-00 00:00:00');
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +135,24 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`id_barang_keluar`);
+
+--
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_barang_masuk`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +161,24 @@ ALTER TABLE `barang`
 --
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
