4feb065c68f4e28e55fc67282d50c49a488d4d09
Dwi Cahyono <cahyonodwi466@gmail.com>
2023-07-31 15:09:28 +0700

    update home

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,5 +1,5 @@
 <?php $this->extend('layout/main'); ?>
 
 <?= $this->section('content'); ?>
-<img src="<?= base_url() ?>/assets/img/hello.svg" alt="">
+<img class="img-fluid" src="<?= base_url() ?>/assets/img/hello.svg" alt="">
 <?= $this->endSection(); ?>
